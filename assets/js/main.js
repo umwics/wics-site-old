110b918166ff47fc9e2fe01b1c6a0201384425f9
tiffcj <tiffanyjiang598@gmail.com>
2017-11-18 13:47:18 -0600

    Make footer a sticky footer

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function positionFooter() {
+    var docHeight = $(document).height();
+    var viewportHeight = $(window).height();
+
+    if (docHeight <= viewportHeight) {
+        $('#footer-navbar').addClass('navbar-fixed-bottom');
+    }
+}
