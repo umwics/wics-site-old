47e2598f5798bf52d4a4622048c8733af7616ee6
Evan Madill <madille@myumanitoba.ca>
2020-05-28 05:57:54 -0500

    add member

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,11 @@
 $(".main-carousel").flickity({
-	// options
-	contain: true,
-	// disable previous & next buttons and dots
-	prevNextButtons: false,
-	autoPlay: 5000,
-	wrapAround: true,
-	draggable: false,
-	pageDots: true,
-	pauseAutoPlayOnHover: false
+  // options
+  contain: true,
+  // disable previous & next buttons and dots
+  prevNextButtons: false,
+  autoPlay: 5000,
+  wrapAround: true,
+  draggable: false,
+  pageDots: true,
+  pauseAutoPlayOnHover: false
 });
